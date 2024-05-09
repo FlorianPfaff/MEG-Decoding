2d625bb787103e4517bf76fb29e1485406e2137f
Florian Pfaff <florian.s.pfaff@gmail.com>
2024-05-09 21:29:38 +0200

    Separated preprocessing steps from rest

---
+++ evaluateModelTransfer.m
@@ -0,0 +1,29 @@
+function accuracies = evaluateModelTransfer(dataFolder, parts, nFolds, windowSize, trainWindowCenter, ...
+    nullWindowCenter, newFramerate, classifier, classifierParam, componentsPCA, frequencyRange)
+
+arguments
+    dataFolder char = '.';
+    parts (1, :) double {mustBeInteger, mustBePositive} = [2]
+    nFolds (1, 1) double {mustBeInteger, mustBePositive} = 10;
+    % Window size in seconds
+    windowSize (1, 1) double = 0.1;
+    % All times are relative to the onset of the stimulus
+    % If a single number is given, only one point in time is used.
+    % Use two numbers to specify a time window, e.g., [0.2, 0.3].
+    % Original was 0.2
+    trainWindowCenter (1, 1) double = 0.2;
+    % Set to nan for disabling extraction of null data. Default: -0.2
+    nullWindowCenter (1, 1) double = -0.2;
+    % Set newFramerate to inf to disable downsampling. Set to '100' to emulate the behavior of runLasso.m
+    newFramerate (1, 1) double = inf;
+    % Type of classifier to use, e.g. 'lasso', 'multiclass-svm', 'random-forest', 'gradient-boosting', 'knn', 'mostFrequentDummy', 'always1Dummy'.
+    classifier char = 'multiclass-svm';
+    % Param of L1 regularisation of Lasso GLM or box constraint of SVM,
+    % number of trees for RF, number of boosting iterations for GBM,
+    % of neighbors for KNN, etc.
+    classifierParam (1, 1) double = nan;
+    % Number of components to retain after PCA. Set to inf to keep all components.
+    componentsPCA (1, 1) double = 100;
+    frequencyRange (1, 2) double = [0, inf];
+end
+
--- runDecoding.m
+++ runDecoding.m
@@ -31,10 +31,6 @@ function accuracies = runDecoding(dataFolder, parts, nFolds, windowSize, trainWi
         frequencyRange (1, 2) double = [0, inf];
     end
 
-    trainWindow = trainWindowCenter + [-windowSize / 2, windowSize / 2];
-    nullTimeWindow = nullWindowCenter + [-windowSize / 2, windowSize / 2];
-    assert(any(isnan(nullTimeWindow)) || nullTimeWindow(end) <= trainWindow(1), 'Null window must be before train window')
-
     if any(isnan(classifierParam))
         classifierParam = getDefaultClassifierParam(classifier);
     end
@@ -45,28 +41,18 @@ function accuracies = runDecoding(dataFolder, parts, nFolds, windowSize, trainWi
 
     %% Loop through each participant
     for p = 1:length(parts)
-
         % Load the data for that participant
         load([dataFolder filesep 'Part' int2str(parts(p)) 'Data.mat'], 'data');
-
-        % Filter the data
-        data = filterData(data, frequencyRange(1), frequencyRange(2));
-        % Downsample the data, if required
-        if newFramerate ~= inf
-            data = downsampleData(data, newFramerate);
-        end
-
         % Extract some basic information
         labels = data.trialinfo;
         nTrials = length(labels);
         nStim = length(unique(labels));
-
-        [stimuliFeaturesCell, nullFeaturesCell] = extractWindows(data, trainWindow, nullTimeWindow);
+        [stimuliFeaturesCell, nullFeaturesCell] = preprocessFeatures(data, frequencyRange, newFramerate, windowSize, trainWindowCenter, nullWindowCenter);
         allFeatures = horzcat(stimuliFeaturesCell{:}, nullFeaturesCell{:});
 
         % Create folds
         fold = ceil((1:length(data.trial)) / (length(data.trial) / nFolds));
-        if diff(nullTimeWindow)~=0
+        if ~isempty(nullFeaturesCell)
             % Assing null data to the same fold as the corresponding stimulus data
             fold = [fold, fold];
             labels = [labels, zeros(1, length(data.trial))];
@@ -164,54 +150,6 @@ function accuracies = runDecoding(dataFolder, parts, nFolds, windowSize, trainWi
 
 end
 
-function data = downsampleData(data, newFramerate)
-    % Downsample the MEG data to a new sampling frequency if required
-
-    % Determine the original sampling frequency
-    rawFs = round(1 / median(diff(data.time{1})));
-
-    % Only proceed if newFramerate is different from rawFs
-    if ~isempty(newFramerate) && rawFs ~= newFramerate
-        % Define new time vector based on newFramerate
-        newT = data.time{1}(1):1 / newFramerate:data.time{1}(end);
-
-        % Loop through each trial to downsample
-        for t = 1:length(data.trial)
-            % Detrend and interpolate data for this trial
-            data.trial{t} = detrend(data.trial{t}')'; % Detrend data before interpolation
-            data.trial{t} = interp1(data.time{t}, data.trial{t}', newT)'; % Interpolate data
-            data.time{t} = newT; % Update time vector for this trial
-        end
-    end
-end
-
-function [stimuliFeaturesCell, nullFeaturesCell] = extractWindows(data, trainWindow, nullTimeWindow)
-    arguments
-        data struct
-        trainWindow (1, 2) double
-        nullTimeWindow (1, 2) double
-    end
-
-    % Partition the data into blocks, identify training time and null time
-    assert(any(isnan(nullTimeWindow)) || diff(nullTimeWindow) == diff(trainWindow), 'Train and null window must have the same length')
-    assert(diff(trainWindow) >= 0, 'Train window ill defined')
-
-    
-    [~, trainBeginIndex] = min(abs(data.time{1} - trainWindow(1)));
-    [~, trainEndIndex] = min(abs(data.time{1} - trainWindow(2)));
-    stimuliFeaturesCell = cellfun(@(x) reshape(x(:, trainBeginIndex:trainEndIndex), [], 1), data.trial, 'UniformOutput', false);
-
-    if any(isnan(nullTimeWindow))
-        nullFeaturesCell = {};
-    elseif diff(nullTimeWindow) >= 0
-        [~, nullBeginIndex] = min(abs(data.time{1} - nullTimeWindow(1)));
-        nullEndIndex = nullBeginIndex + (trainEndIndex - trainBeginIndex);
-        nullFeaturesCell = cellfun(@(x) reshape(x(:, nullBeginIndex:nullEndIndex), [], 1), data.trial, 'UniformOutput', false);
-    else
-        error('Invalid null window')
-    end
-end
-
 function model = trainGradientBoosting(trainFeatures, trainLabels, classifierParam)
     % Train the Gradient Boosting model
     t = templateTree('MaxNumSplits', 20); % Customize decision tree template
@@ -252,43 +190,5 @@ function [reducedFeatures, coeff, explainedVariance] = reduceFeaturesPCA(feature
 
     % Calculate the explained variance of the selected components
     explainedVariance = sum(explained(1:nComponents));
-
-end
-
-function newData = filterData(data, lowFreq, highFreq)
-    newData = data; % Initialize newData with the input data structure
-
-    % Determine the sampling rate from the time vector of the first trial
-    % Assumes uniform sampling rate across all trials
-    if ~isempty(data.time{1})
-        Fs = 1 / mean(diff(data.time{1}));
-    else
-        error('Time vector is empty or not provided correctly.');
-    end
-    assert(lowFreq>=0, 'Low frequency must be greater than or equal to 0')
-    assert(highFreq>=0, 'High frequency must be greater than or equal to 0')
-    assert(highFreq>=lowFreq, 'High frequency must be greater than or equal to low frequency')
-    % Design the filter based on the input frequencies
-    if lowFreq == 0 && highFreq == inf
-        % Nothing to do
-        return
-    elseif lowFreq == 0 && highFreq ~= inf
-        % Lowpass filter design
-        [b, a] = butter(4, highFreq / (Fs / 2), 'low');
-    elseif lowFreq ~= 0 && highFreq ~= inf
-        % Bandpass filter design
-        [b, a] = butter(4, [lowFreq highFreq] / (Fs / 2), 'bandpass');
-    else
-        error("Highpass filter not supported.")
-    end
-
-    % Apply the designed filter to each trial
-    for i = 1:length(data.trial)
-        % Apply the filter
-        % Transpose data.trial{i} to match filtfilt dimension requirements (time dimension first)
-        newData.trial{i} = filtfilt(b, a, data.trial{i}.');
-        % Transpose back to original dimensions (sensors, timesteps)
-        newData.trial{i} = newData.trial{i}.';
-    end
 
 end
